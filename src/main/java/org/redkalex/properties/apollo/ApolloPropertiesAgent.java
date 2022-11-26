c75a956229fe9a718bc046145e843e7ba5b2a49b
Redkale <redkale@qq.com>
2022-11-26 13:41:24 +0800

    ApolloPropertiesAgent鏀寔澶歯amespace

--- src/main/java/org/redkalex/properties/apollo/ApolloPropertiesAgent.java
+++ src/main/java/org/redkalex/properties/apollo/ApolloPropertiesAgent.java
@@ -4,7 +4,7 @@ package org.redkalex.properties.apollo;
 
 import com.ctrip.framework.apollo.*;
 import com.ctrip.framework.apollo.core.ConfigConsts;
-import java.util.Properties;
+import java.util.*;
 import java.util.logging.Level;
 import org.redkale.boot.*;
 import org.redkale.util.*;
@@ -34,8 +34,11 @@ public class ApolloPropertiesAgent extends PropertiesAgent {
 
     @Override
     public void init(final Application application, final AnyValue propertiesConf) {
+        //可系统变量:  apollo.appid、apollo.meta、apollo.cluster、apollo.label、apollo.namespace
+        Properties agentConf = new Properties();
         propertiesConf.forEach((k, v) -> {
             String key = k.replace('-', '.').replace('_', '.');
+            agentConf.put(key, v);
             if (key.equals("apollo.appid")) key = "apollo.app.id";
             if (key.startsWith("apollo.") && System.getProperty(key) == null) {
                 if (key.startsWith("apollo.app.")) {
@@ -46,24 +49,27 @@ public class ApolloPropertiesAgent extends PropertiesAgent {
         });
         //远程请求具体类: com.ctrip.framework.apollo.internals.RemoteConfigRepository
         //String cluster = System.getProperty(ConfigConsts.APOLLO_CLUSTER_KEY, ConfigConsts.CLUSTER_NAME_DEFAULT);
-        String namespace = propertiesConf.getOrDefault("namespace", ConfigConsts.NAMESPACE_APPLICATION);
-        Config config = ConfigService.getConfig(namespace);
-        logger.log(Level.FINER, "apollo config size: " + config.getPropertyNames().size());
-        config.addChangeListener(changeEvent -> {
-            Properties props = new Properties();
-            changeEvent.changedKeys().forEach(k -> {
-                String val = changeEvent.getChange(k).getNewValue();
-                props.put(k, val);
+        String namespaces = agentConf.getProperty("apollo.namespace", ConfigConsts.NAMESPACE_APPLICATION); //多个用,分隔
+        for (String namespace : namespaces.split(";|,")) {
+            if (namespace.trim().isEmpty()) continue;
+            Config config = ConfigService.getConfig(namespace.trim());
+            logger.log(Level.FINER, "apollo config size: " + config.getPropertyNames().size());
+            config.addChangeListener(changeEvent -> {
+                Properties props = new Properties();
+                changeEvent.changedKeys().forEach(k -> {
+                    String val = changeEvent.getChange(k).getNewValue();
+                    props.put(k, val);
+                });
+                //更新全局配置项
+                putResourceProperties(application, props);
             });
-            //更新全局配置项
-            putResourceProperties(application, props);
-        });
-        //初始化配置项
-        config.getPropertyNames().forEach(k -> {
-            String val = config.getProperty(k, null);
-            //更新全局配置项
-            putResourceProperties(application, k, val);
-        });
+            //初始化配置项
+            config.getPropertyNames().forEach(k -> {
+                String val = config.getProperty(k, null);
+                //更新全局配置项
+                putResourceProperties(application, k, val);
+            });
+        }
     }
 
     @Override
