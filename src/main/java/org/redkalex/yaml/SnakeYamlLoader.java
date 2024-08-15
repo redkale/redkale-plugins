516b3be7a89cff99efa45e81be6648a05508ca4b
redkale <redkale@qq.com>
2024-08-15 11:07:21 +0800

    SnakeYamlLoader

--- src/main/java/org/redkalex/yaml/SnakeYamlLoader.java
+++ src/main/java/org/redkalex/yaml/SnakeYamlLoader.java
@@ -4,8 +4,11 @@
 
 package org.redkalex.yaml;
 
+import java.util.Collection;
 import java.util.Map;
+import org.redkale.convert.json.JsonConvert;
 import org.redkale.util.AnyValue;
+import org.redkale.util.AnyValueWriter;
 import org.redkale.util.YamlProvider;
 import org.yaml.snakeyaml.Yaml;
 
@@ -24,6 +27,40 @@ public class SnakeYamlLoader implements YamlProvider.YamlLoader {
     @Override
     public AnyValue read(String content) {
         Map<String, Object> map = new Yaml().load(content);
-        throw new UnsupportedOperationException("Not supported yaml.");
+        AnyValueWriter writer = AnyValue.create();
+        read(writer, map);
+        return writer;
+    }
+
+    protected static void read(AnyValueWriter writer, Map<String, Object> map) {
+        map.forEach((k, v) -> {
+            if (v == null) {
+                writer.addValue(k, (String) null);
+            } else if (v instanceof Map) {
+                AnyValueWriter item = AnyValue.create();
+                writer.addValue(k, item);
+                read(item, (Map) v);
+            } else if (v instanceof Collection) {
+                read(writer, k, (Collection) v);
+            } else {
+                writer.addValue(k, JsonConvert.root().convertTo(v));
+            }
+        });
+    }
+
+    protected static void read(AnyValueWriter writer, String k, Collection list) {
+        for (Object v : list) {
+            if (v == null) {
+                writer.addValue(k, (String) null);
+            } else if (v instanceof Map) {
+                AnyValueWriter item = AnyValue.create();
+                writer.addValue(k, item);
+                read(item, (Map) v);
+            } else if (v instanceof Collection) {
+                read(writer, k, (Collection) v);
+            } else {
+                writer.addValue(k, JsonConvert.root().convertTo(v));
+            }
+        }
     }
 }
