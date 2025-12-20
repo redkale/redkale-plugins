10eb41acbef60bbf5d37f8d10e467319cf3ca873
redkale <redkale@qq.com>
2025-12-20 11:46:47 +0800

    更新publish-pom

--- my/gitrun.sh
+++ my/gitrun.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export LC_ALL="zh_CN.UTF-8"
+export MAVEN_GPG_PASSPHRASE=GPG密码
 
 rm -fr redkale-plugins
 
@@ -9,6 +10,7 @@ rm -fr src
 git clone https://github.com/redkale/redkale-plugins.git
 
 cp -fr redkale-plugins/src ./
+
 
 mvn clean
 mvn deploy
